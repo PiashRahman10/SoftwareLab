93a8298fed5c97816177778ba2a5cffbdf1c0008
Piash Rahman <naimurrahman1952@gmail.com>
2025-01-27 01:21:37 +0600

    change in appoint page

--- appointment.php
+++ appointment.php
@@ -114,7 +114,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
             }
         }
     }
-    
 }
 ?>
 
@@ -140,8 +139,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
 </head>
 
 <body>
-    <!-- Topbar Start -->
-    <div class="container-fluid bg-light p-0 wow fadeIn" data-wow-delay="0.1s">
+     <!-- Topbar Start -->
+     <div class="container-fluid bg-light p-0 wow fadeIn" data-wow-delay="0.1s">
         <div class="row gx-0 d-none d-lg-flex">
             <div class="col-lg-7 px-5 text-start">
                 <div class="h-100 d-inline-flex align-items-center py-3 me-4">
@@ -182,17 +181,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
             <a href="aboutus.php" class="nav-item nav-link ">About</a>
             
             <a href="mediator.php" class="nav-item nav-link">Mediator</a>
-            <a href="arbitrator.php" class="nav-item nav-link ">Arbitrator</a>
+            <a href="arbitrator.php" class="nav-item nav-link">Arbitrator</a>
             <a href="querry.php" class="nav-item nav-link">Query</a>
             <div class="nav-item dropdown">
                 <a href="#" class="nav-link dropdown-toggle active" data-bs-toggle="dropdown">Service</a>
                 <div class="dropdown-menu rounded-0 rounded-bottom m-0">
-                    <a href="Arbitration_proposal.php" class="dropdown-item ">Arbitration Proposal</a>
+                    <a href="Arbitration_proposal.php" class="dropdown-item">Arbitration Proposal</a>
                     <a href="Arbitration.php" class="dropdown-item">Arbitration Case File</a>
                     <a href="mediation_proposal.php" class="dropdown-item">Mediation Proposal</a>
                     <a href="mediation.php" class="dropdown-item">Mediation Case File</a>
                     <a href="others.php" class="dropdown-item">Service Information</a>
-                    <a href="lawyer.php" class="dropdown-item active">Lawyers Info</a>
+                    <a href="lawyer.php" class="dropdown-item">Lawyers Info</a>
                     
                 </div>
             </div>
@@ -201,17 +200,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
         <a href="lawyer_registration.php" class="btn btn-primary rounded-0 py-4 px-lg-5 d-none d-lg-block">Register<i class="fa fa-arrow-right ms-3"></i><br>as lawyer</a>
     </div>
 </nav>
-    <!-- Navbar End -->
+ <!-- Navbar End -->
 
-    <!-- Page Header Start -->
-    <div class="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
+    <style>
+    .page-header {
+    background: url("header-page.jpg") top center no-repeat;
+    background-size: cover;
+    text-shadow: 0 0 30px rgba(0, 0, 0, .1);
+}
+</style>
+   <!-- Page Header Start -->
+   <div class="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
         <div class="container py-5">
-            <h1 class="display-3 text-white mb-3 animated slideInDown">Lawyer Appointment</h1>
+            <h1 class="display-3 text-white mb-3 animated slideInDown">Book Appointment</h1>
             <nav aria-label="breadcrumb animated slideInDown">
                 <ol class="breadcrumb text-uppercase mb-0">
                     <li class="breadcrumb-item"><a class="text-white" href="#">Home</a></li>
                     <li class="breadcrumb-item"><a class="text-white" href="#">Pages</a></li>
-                    <li class="breadcrumb-item text-primary active" aria-current="page">Lawyer Appointment</li>
+                    <li class="breadcrumb-item text-primary active" aria-current="page">Book Appointment</li>
                 </ol>
             </nav>
         </div>
@@ -331,23 +337,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
 
     
     <!-- Footer Start -->
-    <div class="container-fluid bg-dark text-light footer mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s">
+    <div class="container-fluid bg-dark text-light footer pt-5 wow fadeIn" data-wow-delay="0.1s">
         <div class="container py-5">
             <div class="row g-5">
                 <div class="col-lg-3 col-md-6">
                     <h5 class="text-light mb-4">Address</h5>
-                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Baridhara, Gulshan-2, Dhaka-1212</p>
-                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+88 01780337775</p>
-                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>alliance247@gmail.com</p>
-                    
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <h5 class="text-light mb-4">Services</h5>
-                    <a class="btn btn-link" href="">Arbitration</a>
-                    <a class="btn btn-link" href="">Mediation</a>
-                    <a class="btn btn-link" href="">Lawyers</a>
-                    <a class="btn btn-link" href="">Arbitration case file</a>
-                    <a class="btn btn-link" href="">Mediation Case File</a>
+                    <p><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
+                    <p><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
+                    <p><i class="fa fa-envelope me-3"></i>info@example.com</p>
                 </div>
                 <div class="col-lg-3 col-md-6">
                     <h5 class="text-light mb-4">Quick Links</h5>
@@ -357,20 +354,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
                     <a class="btn btn-link" href="">Terms & Condition</a>
                     <a class="btn btn-link" href="">Support</a>
                 </div>
-                <!-- most right text area-->
                 <div class="col-lg-3 col-md-6">
-                    <h5 class="text-light mb-4">Follow Us</h5>
-                    <div class="d-flex pt-2">
-                        <a class="btn btn-outline-light btn-social rounded-circle" href=""><i class="fab fa-twitter"></i></a>
-                        <a class="btn btn-outline-light btn-social rounded-circle" href=""><i class="fab fa-facebook-f"></i></a>
-                        <a class="btn btn-outline-light btn-social rounded-circle" href=""><i class="fab fa-youtube"></i></a>
-                        <a class="btn btn-outline-light btn-social rounded-circle" href=""><i class="fab fa-linkedin-in"></i></a>
+                    <h5 class="text-light mb-4">Newsletter</h5>
+                    <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
+                    <div class="position-relative w-100">
+                        <input class="form-control border-0 rounded-pill w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email">
+                        <button type="button" class="btn btn-primary rounded-pill py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
                     </div>
                 </div>
-                <!-- end of most right text area-->
+                <div class="col-lg-3 col-md-6">
+                    <h5 class="text-light mb-4">Follow Us</h5>
+                    <div class="d-flex">
+                        <a class="btn btn-square btn-outline-light rounded-circle me-2" href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-square btn-outline-light rounded-circle me-2" href="#"><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-square btn-outline-light rounded-circle me-2" href="#"><i class="fab fa-linkedin-in"></i></a>
+                        <a class="btn btn-square btn-outline-light rounded-circle me-2" href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
             </div>
         </div>
-        
     </div>
     <!-- Footer End -->
 
@@ -382,6 +384,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
     <!-- Template Javascript -->
     <script src="js/main.js"></script>
 </body>
-
 </html>
-
